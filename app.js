d4077f95af6518d71e2ba6b44aab1a18da866154
unknown <he1275765531@sohu.com>
2017-11-07 17:53:01 +0800

    'session'

--- app.js
+++ app.js
@@ -27,14 +27,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-    secret: 'sessionID',
-    resave: true,
-    saveUninitialized: true,
+    name: 'sessionID',// 设置 cookie 中保存 session id 的字段名称
+    secret: "sessionID",// 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+    resave: true,// 强制更新 session
+    saveUninitialized: false,// 设置为 false，强制创建一个 session，即使用户未登录
     cookie: {
-        maxAge: 60 * 60 * 100
+        maxAge: 60 * 60 * 100// 过期时间，过期后 cookie 中的 session id 自动删除
     },
-    store: new MongoStore({
-        url: config.mongodb
+    store: new MongoStore({// 将 session 存储到 mongodb
+        url: config.mongodb// mongodb 地址
     })
 }));
 app.use('/', index);
--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,13 @@ var express = require('express');
 var User = require('../models').User;
 var crypto = require('crypto');
 var router = express.Router();
-function encrypt(str) {
+encrypt = (str) => {
     return crypto.createHash('md5').update(str).digest('hex')
 }
-router.post('/reg', function (req, res) {
+router.post('/reg', (req, res)=> {
     var user = req.body;
     user.password = encrypt(user.password)
-    new User(user).save(function (err, user) {
+    new User(user).save((err, user)=> {
         if (err) {
             res.status(500).json({msg: err});
         } else {
@@ -25,11 +25,11 @@ router.post('/login', function (req, res) {
         } else {
             req.session.user = user;
             /* res.json(user)*/
-            res.render('header.ejs', {data: user})
+            res.render('header', {data: user})
         }
     })
 });
-router.get('/logout', function (req, res) {
+router.get('/logout', (req, res)=> {
     req.session.user = null;
     //res.status(200).json({msg: 'success'})
     res.redirect('/')
